7a5272de131a28b9e5ba812fff0d87660d138d2c
Hyun Kong <konghyunsik7@gmail.com>
2022-07-05 12:00:33 -0400

    Excerise#2

---
+++ MethodsExcerise2/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExcerise2
+{
+    internal class Calc
+    {
+        public static int Sum(int x,int y)
+        {
+            return x + y;
+        }
+        public static int Multi(int x, int y)
+        {
+            return x * y;        
+        }
+        public static int sub(int x, int y)
+
+        {
+            return x - y;
+        }
+        public static int div(int x, int y)
+        {
+            return x / y;
+
+        }
+
+        public static int Sums(int a, int b, int c, int d, int e)
+        {
+            return a + b + c + d + e;
+        }
+        
+    }
+}
---
+++ MethodsExcerise2/Program.cs
@@ -0,0 +1,8 @@
+﻿using MethodsExcerise2;
+
+
+Console.WriteLine(Calc.Sum(2,4));
+Console.WriteLine(Calc.Multi(10,2));
+Console.WriteLine(Calc.div(25,5));
+Console.WriteLine(Calc.sub(100,99));
+Console.WriteLine(Calc.Sums(1,2,3,4,5));
